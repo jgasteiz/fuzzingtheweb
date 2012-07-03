f59ee9c3cb57955e186a6e4cac74d3ff6eea8059
Javi Manzano <javigasteiz89@gmail.com>
2012-07-03 21:43:39 +0200

    changed tabs for spaces

--- fuzzopress/urls.py
+++ fuzzopress/urls.py
@@ -5,15 +5,15 @@ admin.autodiscover()
 from fuzzopress.blog.views import BlogView, BlogPostView, AboutView, ArchiveDay, ArchiveMonth, ArchiveYear
 
 urlpatterns = patterns('',
-	url(r'^$',
-		BlogView.as_view(),
-		name='blog'),
-	url(r'^admin/', include(admin.site.urls)),
-	url(r'^about-fuzzopress/',
-		AboutView.as_view(
-			template_name = "about/aboutfuzzopress.html"),
-		name='aboutfuzzopress'),
-	url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',
+    url(r'^$',
+        BlogView.as_view(),
+        name='blog'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^about-fuzzopress/',
+        AboutView.as_view(
+            template_name = "about/aboutfuzzopress.html"),
+        name='aboutfuzzopress'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',
         ArchiveDay.as_view(),
         name='archive_day'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$',
@@ -23,6 +23,6 @@ urlpatterns = patterns('',
         ArchiveYear.as_view(),
         name='archive_year'),
     url(r'^(?P<slug>[-\w]+)$',
-		BlogPostView.as_view(),
-		name='post'),
+        BlogPostView.as_view(),
+        name='post'),
 )
