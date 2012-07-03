e6a47a884c266338cd96c3b0f9945914731f9f3f
Javi Manzano <javigasteiz89@gmail.com>
2012-07-03 21:43:01 +0200

    Added USE_TZ = True

--- fuzzopress/settings.py
+++ fuzzopress/settings.py
@@ -29,6 +29,8 @@ DATABASES = {
 # system time zone.
 TIME_ZONE = 'America/Chicago'
 
+USE_TZ = True
+
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
