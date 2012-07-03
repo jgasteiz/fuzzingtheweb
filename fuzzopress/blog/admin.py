dc8025d1fb2f449404fa5f3e6e8480a5ce5d6efe
Javi Manzano <javigasteiz89@gmail.com>
2012-07-03 21:38:05 +0200

    Changed tabs for spaces

--- fuzzopress/blog/admin.py
+++ fuzzopress/blog/admin.py
@@ -4,17 +4,17 @@ from markitup.widgets import AdminMarkItUpWidget
 from fuzzopress.blog.models import Post, NavItem, Widget
 
 class PostAdmin(admin.ModelAdmin):
-	list_display = ('id', 'title', 'slug', 'body', 'created', 'updated_at', 'published', 'draft')
-	def formfield_for_dbfield(self, db_field, **kwargs):
-		if db_field.name == 'body':
-			kwargs['widget'] = AdminMarkItUpWidget()
-		return super(PostAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+    list_display = ('id', 'title', 'slug', 'body', 'created', 'updated_at', 'published', 'draft')
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'body':
+            kwargs['widget'] = AdminMarkItUpWidget()
+        return super(PostAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
 class NavItemAdmin(admin.ModelAdmin):
-	list_display = ('id', 'name', 'url', 'weight')
+    list_display = ('id', 'name', 'url', 'weight')
 
 class WidgetAdmin(admin.ModelAdmin):
-	list_display = ('id', 'name', 'body', 'weight')
+    list_display = ('id', 'name', 'body', 'weight')
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(NavItem, NavItemAdmin)
