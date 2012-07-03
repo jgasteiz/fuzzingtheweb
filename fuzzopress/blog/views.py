8dad578efdcd0d02afefdf1c577ce1c79bcdf6df
Javi Manzano <javigasteiz89@gmail.com>
2012-07-03 21:42:50 +0200

    Added archive to the main context

--- fuzzopress/blog/views.py
+++ fuzzopress/blog/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.views.generic.dates import YearArchiveView, MonthArchiveView, DayArchiveView
 from django.views.generic import ListView, DetailView, TemplateView
-from django.shortcuts import get_object_or_404
 from fuzzopress.blog.models import Post, NavItem, Widget
+from django.shortcuts import get_object_or_404
+from datetime import date
 
 class CustomContextMixin(object):
     """
@@ -10,7 +11,21 @@ class CustomContextMixin(object):
     """
     def get_context_data(self, **kwargs):
         context = super(CustomContextMixin, self).get_context_data(**kwargs)
-        context.update({ 'navItems': NavItem.objects.all(), 'widgets': Widget.objects.all() })
+        arch = Post.objects.dates('published', 'month', order='DESC')
+        archives = {}
+        for i in arch:
+            year = i.year
+            month = i.month
+            try:
+                archives[year][month-1][1] = True
+            except KeyError:
+                # catch the KeyError, and set up list for that year
+                archives[year] = [[date(year, m ,1),False] for m in xrange(1,13)]
+                archives[year][month-1][1] = True
+        context.update({
+            'archives': sorted(archives.items(),reverse=True),
+            'navItems': NavItem.objects.all(), 
+            'widgets': Widget.objects.all() })
         return context
 
 class BlogPostView(CustomContextMixin, DetailView):
@@ -42,10 +57,8 @@ class CustomDateMixin(CustomContextMixin, object):
     Same date_field and context_object_name for every Dated-class view
     """
     date_field = 'published'
+    model = Post
     context_object_name = 'post_list'
-
-    def get_queryset(self):
-        return Post.objects.published()
 
 class ArchiveDay(CustomDateMixin, DayArchiveView):
     """
