71e89af9bb0c7b0fa966045dfa94a1814d113c6c
Javi Manzano Oller <javi.manzano.oller@gmail.com>
2012-07-21 15:24:41 +0200

    Fixed sidebar issue

--- fuzzopress/blog/static/js/main.js
+++ fuzzopress/blog/static/js/main.js
@@ -1,42 +1,44 @@
 /*
- *	JavaScript code for the sidebar-hider
+ *  JavaScript code for the sidebar-hider
  *
  */
 $(document).ready(function() {
-	var SIDEBAR_SHOWN = true;
-	$('.toggle-sidebar').click(function() {
-		if (SIDEBAR_SHOWN === true) {
-			SIDEBAR_SHOWN = false;
-			$('.articles').addClass('width-max');
-			$('.toggle-sidebar span').html('<<');
-			$('.sidebar').removeClass('open');
-			$('.sidebar').addClass('close');
-			$('.sidebar-content').hide();
-		}
-		else {
-			SIDEBAR_SHOWN = true;
-			$('.articles').removeClass('width-max');
-			$('.toggle-sidebar span').html('>>');
-			$('.sidebar').removeClass('close');
-			$('.sidebar').addClass('open');
-			$('.sidebar-content').show();
-		}
-	});
-	$('.light-switch').click(function() {
-		$('body, nav, aside, .toggle-sidebar, .main-content, footer.main.')
-			.toggleClass('night-mode');
-	});
 
-	$('.fuzz-finder').keypress(function(e) {
-		if (e.charCode === 13) {
-			var inputText = $(this).val().replace(/ /g, '+');
-			location.href = '/search/' + inputText + '/';
-		}
-	});
-	$('.fuzz-finder-go').click(function() {
-		var inputText = $('.fuzz-finder').val().replace(/ /g, '+');
-		if (inputText !== '') {
-			location.href = '/search/' + inputText + '/';
-		}
-	});
+    var SIDEBAR_SHOWN = true;
+
+    $('.toggle-sidebar').click(function() {
+        if (SIDEBAR_SHOWN === true) {
+            SIDEBAR_SHOWN = false;
+            $('#content').addClass('width-max');
+            $('.toggle-sidebar span').html('<<');
+            $('.sidebar, .toggle-sidebar').addClass('close');
+            $('.sidebar-content').hide();
+        }
+        else {
+            SIDEBAR_SHOWN = true;
+            $('#content').removeClass('width-max');
+            $('.toggle-sidebar span').html('>>');
+            $('.sidebar, .toggle-sidebar').removeClass('close');
+            $('.sidebar-content').show();
+        }
+    });
+
+    $('.light-switch').click(function() {
+        $('body, nav, aside, .toggle-sidebar, #main, #content, footer.main.')
+            .toggleClass('night-mode');
+    });
+
+    $('.fuzz-finder').keypress(function(e) {
+        if (e.charCode === 13) {
+            var inputText = $(this).val().replace(/ /g, '+');
+            location.href = '/search/' + inputText + '/';
+        }
+    });
+
+    $('.fuzz-finder-go').click(function() {
+        var inputText = $('.fuzz-finder').val().replace(/ /g, '+');
+        if (inputText !== '') {
+            location.href = '/search/' + inputText + '/';
+        }
+    });
 });
