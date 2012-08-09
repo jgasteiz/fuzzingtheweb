e033ec5523f3d664fd36a3202f651ee1d0173d42
jgasteiz <javi.manzano.oller@gmail.com>
2012-08-09 18:05:34 +0200

    pass jslint
    it made me cry a bit :(

--- fuzzopress/blog/static/js/main.js
+++ fuzzopress/blog/static/js/main.js
@@ -1,20 +1,22 @@
+/*global $, jQuery, document, location*/
+
 /*
  *  JavaScript code for the sidebar-hider
  *
  */
-$(document).ready(function() {
+$(document).ready(function () {
+    'use strict';
 
     var SIDEBAR_SHOWN = true;
 
-    $('.toggle-sidebar').click(function() {
+    $('.toggle-sidebar').click(function () {
         if (SIDEBAR_SHOWN === true) {
             SIDEBAR_SHOWN = false;
             $('#content').addClass('width-max');
             $('.toggle-sidebar span').html('<<');
             $('.sidebar, .toggle-sidebar').addClass('close');
             $('.sidebar-content').hide();
-        }
-        else {
+        } else {
             SIDEBAR_SHOWN = true;
             $('#content').removeClass('width-max');
             $('.toggle-sidebar span').html('>>');
@@ -23,19 +25,19 @@ $(document).ready(function() {
         }
     });
 
-    $('.light-switch').click(function() {
+    $('.light-switch').click(function () {
         $('body, nav, aside, .toggle-sidebar, #main, #content, footer.main.')
             .toggleClass('night-mode');
     });
 
-    $('.fuzz-finder').keypress(function(e) {
+    $('.fuzz-finder').keypress(function (e) {
         if (e.charCode === 13) {
             var inputText = $(this).val().replace(/ /g, '+');
             location.href = '/search/' + inputText + '/';
         }
     });
 
-    $('.fuzz-finder-go').click(function() {
+    $('.fuzz-finder-go').click(function () {
         var inputText = $('.fuzz-finder').val().replace(/ /g, '+');
         if (inputText !== '') {
             location.href = '/search/' + inputText + '/';
