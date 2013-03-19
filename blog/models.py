c6a55256d9f0eeba9559aca11ab60c207214b224
Javi Manzano <javi@potatolondon.com>
2013-03-19 20:54:12 +0000

    Add gplus and twitter sharing buttons. Will add facebook one when they decide to make it properly

--- blog/models.py
+++ blog/models.py
@@ -99,10 +99,6 @@ class Post(models.Model):
     def __unicode__(self):
         return self.title
 
-    @models.permalink
-    def get_absolute_url(self):
-        return ('post', None, {'slug': self.slug})
-
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title, instance=self)
