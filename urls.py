dcc7df1bc949d996482b73852f0b19da9d7086a1
jgasteiz <javi.manzano.oller@gmail.com>
2013-03-21 23:30:31 +0000

    Reenable admin

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 admin.autodiscover()
 
 urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^news/', include('news.urls')),
     url(r'', include('blog.urls')),
