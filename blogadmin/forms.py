0c617249ba68e3cde5041667b98ff39a7fefccc9
Javi Manzano <javi.manzano.oller@gmail.com>
2013-07-08 22:43:19 +0100

    Add markitup editor to admin textareas

--- blogadmin/forms.py
+++ blogadmin/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from markitup.widgets import AdminMarkItUpWidget
+
 from blog.models import Post
 
 
@@ -15,12 +17,18 @@ class BaseModelForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(BaseModelForm, self).__init__(*args, **kwargs)
         self.set_required_fields()
+        self.set_markitup_fields()
 
     def set_required_fields(self):
         """ Sets the 'required' attribute to 'true' on necessary form fields """
         for field in self.fields:
             if self.fields[field].required:
                 self.fields[field].widget.attrs['required'] = 'true'
+
+    def set_markitup_fields(self):
+        for field in self.fields:
+            if type(self.fields[field].widget) == forms.Textarea:
+                self.fields[field].widget.attrs['class'] = 'markitup-editor'
 
 
 class PostForm(BaseModelForm):
